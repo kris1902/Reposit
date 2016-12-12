a417ba44d4eb527446b799c0dd472ee1023efeee
Krzysztof Dubiel <krzysiekchorzow@gmail.com>
2016-12-12 07:41:37 +0000

    Pierwszy commit

--- srednia.py
+++ srednia.py
@@ -2,10 +2,12 @@ import math
 import myerror
 
 def srednia(liczby):
+	"""Oblicza srednia kwadratowa listy/krotki liczb."""
 	if type(liczby) != list:
 		raise myerror.IBeBack()
 	for i in range(len(liczby)):
 		liczby[i] = float(liczby[i]*liczby[i])
+		print ""
 	wynik = sum(liczby)
 	wynik = math.sqrt(wynik/len(liczby))
 	return wynik
